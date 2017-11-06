9e315d80de71a5b5265f54c7568c7b8097a6c692
arizona2014 <andy.lisac@gmail.com>
2017-11-06 08:04:05 +0100

    Adding functionality

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
+	
+var movementsLeft = 9;
+var computerCurrentTurn = false;
+var computerCode = "X";
+var humanCode = "O";
+
 $(document).ready(function(){
 
+	function writeLetterOnBoard(code, position){
+		switch(position){
+			case "11":
+				$("#cell11").html(code);
+				break;
+			case "12":
+				$("#cell12").html(code);
+				break;
+			case "13":
+				$("#cell13").html(code);
+				break;				
+			case "21":
+				$("#cell21").html(code);
+				break;
+			case "22":
+				$("#cell22").html(code);
+				break;
+			case "23":
+				$("#cell23").html(code);
+				break;								
+			case "31":
+				$("#cell31").html(code);
+				break;
+			case "32":
+				$("#cell32").html(code);
+				break;
+			case "33":
+				$("#cell33").html(code);					
+				break;								
+		}		
+	}
 
 	function findBestPosition(){
 		console.log('Computers turn');
+		console.log(movementsLeft);
+		var bestPosition = "11";
+		writeLetterOnBoard(computerCode,bestPosition);
 	}
 	
-	function switchRole(computerCurrentTurn){
+	function switchRole(){
+		movementsLeft--;
 		computerCurrentTurn = !computerCurrentTurn;	
 		displayTop();
 	}
@@ -14,18 +55,21 @@ $(document).ready(function(){
 		if(computerCurrentTurn){
 			$("#messageTop").html("Computer's turn");
 			findBestPosition();
+			switchRole(computerCurrentTurn);
 		} else {
 			$("#messageTop").html("Human's turn");
 		}
 	}
-	
-	var movementsLeft = 9;
-	var computerCurrentTurn = false;
+
 	displayTop();
 	
     $(".cells").click(function(e){
-		console.log(e.currentTarget.id);
-		switchRole(computerCurrentTurn);
+		if(!computerCurrentTurn && movementsLeft>0){		
+			var FullString = e.currentTarget.id;
+			var position = FullString.slice(-2);
+			writeLetterOnBoard(humanCode,position);
+			switchRole(computerCurrentTurn);
+		}
     });
 	
 });
